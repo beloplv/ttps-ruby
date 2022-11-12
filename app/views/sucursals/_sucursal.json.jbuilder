af019a625686cd0e8f78e691e373ef60a19aac03
beloplv <beloplv.mg@gmail.com>
2022-11-12 11:28:51 -0300

    primer commit

---
+++ app/views/sucursals/_sucursal.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! sucursal, :id, :nombre, :direccion, :telefono, :created_at, :updated_at
+json.url sucursal_url(sucursal, format: :json)
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :sucursals
+  devise_for :usuarios
+  get 'index/home'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "index#home"
+end
